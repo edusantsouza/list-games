6f6a3db779769e0f52b33050d73c8ddf95604057
EduSant <eduardo.santsouza@gmail.com>
2023-06-23 19:02:44 -0300

    feat: resposive and components added

--- src/App.js
+++ src/App.js
@@ -2,20 +2,17 @@
 import './App.css';
 import Header from './Components/Header/index'
 import Content from './Components/Content';
-import React , {useContext, useState}from 'react';
+import React , {useState}from 'react';
 import Context from './Contexts/GeneralContext';
 
 const App = () => {
  const [search, setSearch] = useState('')
-  
-  // const getSearchValue = (value) =>{
-  //   setSearch(value)
-  // } 
+
   return (
     <div className="App">
       <Context.Provider value={[search, setSearch]}>
         <Header />
-        <Content/>
+        <Content />
      </Context.Provider>
     </div>
   );
