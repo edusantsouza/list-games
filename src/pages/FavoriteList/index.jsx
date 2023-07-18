e64be164359c95a5bb77bec969f6295d14922040
EduSant <eduardo.santsouza@gmail.com>
2023-07-18 18:10:33 -0300

    fix: routes

--- src/pages/FavoriteList/index.jsx
+++ src/pages/FavoriteList/index.jsx
@@ -2,12 +2,13 @@ import React, { useContext } from 'react'
 import Sidebar from '../../Components/Sidebar'
 import NavHeader from '../../Components/NavHeader'
 import Spinner from '../../Components/Spinner'
-import { MyContext } from '../../Contexts/GetGameList'
+import { AuthContext } from '../../Contexts/GetGameList'
 import FavoritesListContent from '../../Components/FavoriteListContent'
+import { AuthContext } from '../../Contexts/AuthContext'
 
 
 const FavoriteList = () => {
-  const { favData } = useContext(MyContext)
+  const { favData } = useContext(AuthContext)
 
   return (
     <>
